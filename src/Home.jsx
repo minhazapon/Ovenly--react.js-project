f526a1495447e9bb294cab170aa9cbfd9b1a8d0f
minhazapon <minhajulabedinapon@gmail.com>
2024-12-12 21:02:01 -0800

    added good food file

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import About from "./home file/About";
 import Drinks from "./home file/Drinks";
 import FreshFood from "./home file/FreshFood";
+import GoodFood from "./home file/GoodFood";
 import HomeBanner from "./home file/HomeBanner";
 import House from "./home file/House";
 import Menu from "./home file/Menu";
@@ -19,6 +20,7 @@ const Home = () => {
             <Menu></Menu>
             <OpenHours></OpenHours>
             <Drinks></Drinks>
+            <GoodFood></GoodFood>
             
         </div>
     );
---
+++ src/home file/GoodFood.jsx
@@ -0,0 +1,47 @@
+
+
+
+
+const GoodFood = () => {
+
+    return (
+        <div className=" mt-20 mb-10 ">
+
+            <div className=" text-white p-48 bg-no-repeat bg-cover bg-fixed  "
+            style={{
+                backgroundImage: "url(  https://static.vecteezy.com/system/resources/previews/027/671/365/large_2x/pepperoni-pizza-on-the-wooden-board-with-dark-lighting-and-black-background-food-and-delivery-concept-generative-ai-free-photo.jpg  )",
+              }}
+            >
+
+            
+            
+             <div className=" bg-gradient-to-r from-black p-4 rounded-xl ">
+                
+             <p className=" text-center nav text-2xl">Visit Us</p>
+             <p className=" mt-10 text-4xl play text-center ">Good Food and Drinks</p>
+             <p className=" ex text-center font mt-10 ">Donec suscipit lobortis ornare. Aenean vel varius odio.<br></br>
+             Cras ligula odio, congue vel adipiscing non, congue vitae neque.</p>
+             <div className=" mt-10 flex justify-center ">
+             <a href="#_" class="relative inline-block text-lg group">
+             <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+             <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+             <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+             <span class="relative ex ">BOOK NOW</span>
+             </span>
+             <span class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0" data-rounded="rounded-lg"></span>
+             </a>
+             </div>
+
+
+             </div>
+
+
+
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default GoodFood;
